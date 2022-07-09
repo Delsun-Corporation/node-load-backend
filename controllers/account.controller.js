8f923cf8c0f5cdd812869a9f48fb4c87928663d4
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2022-07-09 15:32:46 +0700

    Feat: Update account type

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,5 +1,7 @@
 const { error, success, validation } = require("../helper/baseJsonResponse");
-const Account = require("../models/account.model")
+const Account = require("../models/account.model");
+const User = require("../models/auth.model");
+const _ = require("lodash");
 
 exports.addAccountType = (req, res) => {
     const { name } = req.body;
@@ -20,4 +22,40 @@ exports.addAccountType = (req, res) => {
 
         return res.json(success("Succes Create new Account", null, res.statusCode));
     })
+}
+
+exports.updateAccountType = (req, res) => {
+    const { account_id } = req.body;
+    const { authorization } = req.headers;
+
+    Account.findById(account_id, (err, result) => {
+        if (err) {
+            return res.status(401).json(error("No Account Found with that ID", res.statusCode));
+        }
+
+        User.findOne({
+            token: authorization
+        }, (err, user) => {
+            if (err) {
+                return res.status(401).json(error("Unauthorized", res.statusCode));
+            }
+    
+            const updateAccountId = {
+                account_id
+            }
+      
+            user = _.extend(user, updateAccountId);
+            
+            return user.save((err, result) => {
+                if (err) {
+                    return res
+                      .status(500)
+                      .json(error("Error update account type", res.statusCode));
+                }
+    
+                return res.json(success("Success update account type", null, res.statusCode));
+            })
+        })
+    })
+
 }
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { addAccountType } = require('../controllers/account.controller');
+const { addAccountType, updateAccountType } = require('../controllers/account.controller');
 const router = express.Router();
 
 // POST
 router.post('/account', addAccountType);
+router.post('/update-account-type', updateAccountType);
 
 module.exports = router;
