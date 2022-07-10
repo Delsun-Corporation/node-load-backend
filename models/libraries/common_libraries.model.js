937c0b2038665df2a41a7cc0f0c63a45ccebb22b
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2022-07-10 17:23:09 +0700

    Feat: Create common libraries model

---
+++ models/libraries/common_libraries.model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const commonLibrariesSchema = new mongoose.Schema(
+    {
+        exercise_name: {
+            type: String
+        },
+        user_id: {
+            type: String
+        },
+        category_id: {
+            type: String
+        },
+        regions_ids: {
+            type: [String]
+        },
+        mechanic_id: {
+            type: String
+        },
+        targeted_muscles_id: {
+            type: String
+        },
+        action_force_id: {
+            type: String
+        },
+        equipment_id: {
+            type: String
+        },
+        repetition_max: {
+            type: String
+        },
+        exercise_link: {
+            type: String
+        },
+        is_favorite: {
+            type: Boolean
+        },
+        is_active: {
+            type: Boolean
+        }
+    },
+    {timestamps: true}
+);
+
+module.exports = mongoose.model("common_libraries", commonLibrariesSchema);
