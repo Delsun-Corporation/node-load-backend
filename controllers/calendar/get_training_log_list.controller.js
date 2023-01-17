ccf4019bf1763aa36e5d2d9ad2ce893728e31b0f
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2023-01-17 13:01:41 +0700

    Feat: Get training log detail

--- controllers/calendar/get_training_log_list.controller.js
+++ controllers/calendar/get_training_log_list.controller.js
@@ -2,6 +2,7 @@ const { error, success } = require("../../helper/baseJsonResponse")
 const validateToken = require("../../helper/token_checker")
 const authModel = require("../../models/auth.model")
 const training_log_listModel = require("../../models/calendar/training_log_list.model")
+const ObjectId = require('mongoose').Types.ObjectId;
 
 // return training log list
 // return training log program too
@@ -14,6 +15,20 @@ exports.getTrainingLogList = (req, res) => {
             }
 
             return res.json(success("Success getting training log list", { training_log_list: trainingLogs }, res.statusCode))
+        })
+    })
+}
+
+exports.getTrainingLogDetail = (req, res) => {
+    validateToken(req, res, (user) => {
+        const user_id = user.id;
+        const trainingId = req.params.trainingId;
+        training_log_listModel.findById(trainingId, (err, trainingLog) => {
+            if (err || trainingLog == null) {
+                return res.status(500).json(error("Error find training log list", res.statusCode))
+            }
+
+            return res.json(success("Success getting training log list", { training_log_list: trainingLog }, res.statusCode))
         })
     })
 }
--- routes/calendar.route.js
+++ routes/calendar.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createTrainingLog } = require('../controllers/calendar/create_training_log_list.controller');
 const { getLogCardioValidation } = require('../controllers/calendar/get_log_cardio_validations.controller');
 const { getLogResistanceValidation } = require('../controllers/calendar/get_log_resistance_validations.controller');
-const { getTrainingLogList } = require('../controllers/calendar/get_training_log_list.controller');
+const { getTrainingLogList, getTrainingLogDetail } = require('../controllers/calendar/get_training_log_list.controller');
 const router = express.Router();
 
 // POST
@@ -12,5 +12,6 @@ router.post('/training-log-list', getTrainingLogList);
 // GET
 router.get('/log-cardio-validation-list', getLogCardioValidation);
 router.get('/log-resistance-validation-list', getLogResistanceValidation);
+router.get('/get-training-log/:trainingId', getTrainingLogDetail);
 
 module.exports = router;
